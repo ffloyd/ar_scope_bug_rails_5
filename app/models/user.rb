ed6d976b25b6580ef7e4a0859b1bc60a70f7ba98
Roman Kolesnev <rvkolesnev@gmail.com>
2015-09-24 00:45:08 +0300

    Add with_role and useless_join scopes to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
+
+  has_many :users_roles, dependent: :destroy
+  has_many :roles, -> { distinct }, through: :users_roles
+
+  scope :with_role, lambda { |role_name|
+    joins(:roles).where(roles: { name: role_name })
+  }
+
+  scope :useless_join, lambda {
+    subquery_sql = User.select('users.id AS user_id').to_sql
+    joins("INNER JOIN (#{subquery_sql}) subquery ON subquery.user_id = users.id")
+  }
 end
